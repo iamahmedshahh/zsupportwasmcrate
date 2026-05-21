27fe59eba07b4596d167497c496aa93074bf4aaf
iamahmedshahh <artistahmed41@gmail.com>
2026-05-21 21:08:08 +0500

    == to do ==

--- dist-test.mjs
+++ dist-test.mjs
@@ -4,6 +4,11 @@ const keys = z_getEncryptionAddress({
   seed: new Uint8Array(32).fill(0xaa),
 });
 
-console.log('address length:', keys.address.toString('hex'));  
-console.log('ivk length:', keys.ivk.toString('hex'));          
+console.log('address length:', Buffer.from(keys.address).toString('hex'));   
+console.log('ivk length:', Buffer.from(keys.ivk).toString('hex'));          
 console.log('✓ library works');
+
+
+
+// == to do ==
+//  on javascript we can zeroize manually by just keys.spendingKey.fill(0);  // overwrite with zeroes
